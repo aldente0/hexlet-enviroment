5c7b8e27c72ec818661a9e7600b6fbf6a35323b1
Alexey Lozhkin <leha.lozhkin.02@mail.ru>
2023-09-16 10:22:48 +0300

    add src/Runner.php

---
+++ src/Runner.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace AlDente0\Enviroment\Runner;
+
+function run()
+{
+    $collection = collect(['taylor', 'abigail', null])->map(function ($name) {
+        return strtoupper($name);
+    });
+
+    return $collection;
+}
